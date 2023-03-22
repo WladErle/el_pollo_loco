897d4b9792e5d3dd91b57d7898c8f51c77c04b07
WladErle <wladimir_erlenbusch@hotmail.de>
2023-03-22 19:15:04 +0100

    first commit

---
+++ models/movable-object.class.js
@@ -0,0 +1,12 @@
+class MovableObject {
+    x = 120;
+    y = 400; 
+
+    moveRight() {
+        console.log('Moving right');
+    }
+
+    moveLeft() {
+        console.log('Moving left');
+    }
+}
