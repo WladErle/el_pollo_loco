d5b365d958ba22365b3cecc1c4d6f3be767b0ee6
WladErle <wladimir_erlenbusch@hotmail.de>
2023-03-23 22:04:17 +0100

    third commit

--- models/world.class.js
+++ models/world.class.js
@@ -17,6 +17,20 @@ class World {
     
 
     draw() {
+
+        this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+
         this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height); // 1. 100 steht für breite, 2. 100 steht für höhe.
+        this.enemies.forEach(enemy => {
+            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
+        })
+        
+        
+        //Draw wird immer wieder ausgeführt.
+        let self = this;
+        requestAnimationFrame(function() {
+            self.draw();
+        });
+    
     }
 }
