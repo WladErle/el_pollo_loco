73da6694f5969230ac7e110000dd808be252f47d
WladErle <wladimir_erlenbusch@hotmail.de>
2023-03-23 13:24:08 +0100

    second commit

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,11 @@
 class Character extends MovableObject{
-   
+
+
+   // Constructor: Wird aufgerufen, wenn ein neues Objekt der Klasse erstellt wird. Sollte in jedem einzelnen Objekt vorhanden sein.
+ constructor() {
+    super().loadImage('img_pollo_locco/img/2_character_pepe/1_idle/idle/I-1.png')// super(). : Wird benötigt, um den Constructor von der vererbten Klasse aufzurufen. Erst danach wird der restliche Code im Constructor ausgeführt. Ohne super(). wird die vererbte Klasse nicht korrekt ausgeführt.
+ }
+
     jump() {
 
     }
--- models/world.class.js
+++ models/world.class.js
@@ -7,7 +7,16 @@ class World {
         new Chicken(),
     ]
 
-    draw() {
+    ctx;
 
+    constructor(canvas){
+        this.ctx = canvas.getContext('2d');
+        this.draw();
+    }
+
+    
+
+    draw() {
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height); // 1. 100 steht für breite, 2. 100 steht für höhe.
     }
 }
